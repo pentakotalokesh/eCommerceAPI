22ad96dcf8f76822e5751fee015ac7c51011c507
pentakotalokesh <pentakotalokesh67@gmail.com>
2025-03-22 13:57:04 +0530

    db Configuration

--- ecommerceAPI/settings/production.py
+++ ecommerceAPI/settings/production.py
@@ -5,10 +5,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER':'root',
-        'PASSWORD': 'Lokesh@123',
-        'HOST':'localhost',
+        'NAME': 'drfapis$ecommerce',
+        'USER':'drfapis',
+        'PASSWORD': 'admin@123',
+        'HOST':'drfapis.mysql.pythonanywhere-services.com',
         'PORT':'3306'
     }
 }
